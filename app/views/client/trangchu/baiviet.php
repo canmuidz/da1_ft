25d827567a57e690f2421463472abab5bfff3d9e
canmuidz <150830621+canmuidz@users.noreply.github.com>
2024-07-15 18:50:19 +0700

    first commit

---
+++ app/views/client/trangchu/baiviet.php
@@ -0,0 +1,64 @@
+<!-- =================== BAI VIET ======================= -->
+<section class="from-blog spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title from-blog__title">
+                    <h2>Bài Viết</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="blog__item">
+                    <div class="blog__item__pic">
+                        <img src="./public/assets/dist/img/blog/blog-1.jpg" alt="">
+                    </div>
+                    <div class="blog__item__text">
+                        <ul>
+                            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+                            <li><i class="fa fa-comment-o"></i> 5</li>
+                        </ul>
+                        <h5><a href="#">Mẹo nấu ăn giúp việc nấu ăn trở nên đơn giản</a></h5>
+                        <p>Nhưng bởi vì không bao giờ tìm kiếm được điều gì đó vĩ đại là khó khăn và đau đớn</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="blog__item">
+                    <div class="blog__item__pic">
+                        <img src="./public/assets/dist/img/blog/blog-2.jpg" alt="">
+                    </div>
+                    <div class="blog__item__text">
+                        <ul>
+                            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+                            <li><i class="fa fa-comment-o"></i> 5</li>
+                        </ul>
+                        <h5><a href="#">6 cách chuẩn bị bữa sáng cho 30</a></h5>
+                        <p>Nhưng bởi vì không bao giờ tìm kiếm được điều gì đó vĩ đại là khó khăn và đau đớn</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="blog__item">
+                    <div class="blog__item__pic">
+                        <img src="./public/assets/dist/img/blog/blog-3.jpg" alt="">
+                    </div>
+                    <div class="blog__item__text">
+                        <ul>
+                            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+                            <li><i class="fa fa-comment-o"></i> 5</li>
+                        </ul>
+                        <h5><a href="#">Thăm trang trại sạch ở Mỹ</a></h5>
+                        <p>Nhưng bởi vì không bao giờ tìm kiếm được điều gì đó vĩ đại là khó khăn và đau đớn</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
